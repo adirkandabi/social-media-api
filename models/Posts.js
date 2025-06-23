3f50c5193964530c76f4aa67f8900ebaaf433c1a
adirkandabi <kandabiadir@gmail.com>
2025-06-23 10:02:22 +0300

    latest

--- models/Posts.js
+++ models/Posts.js
@@ -14,10 +14,7 @@ class Posts extends BaseModel {
   }
 
   async update(post_id, updateData) {
-    return this.collection.updateOne(
-      { post_id },
-      { $set: updateData }
-    );
+    return this.collection.updateOne({ post_id }, { $set: updateData });
   }
 
   async delete(post_id) {
@@ -25,7 +22,61 @@ class Posts extends BaseModel {
   }
 
   async list(filter = {}) {
-    return this.collection.find(filter).toArray();
+    return this.collection
+      .aggregate([
+        { $match: filter },
+
+        // Join with users to get first_name
+        {
+          $lookup: {
+            from: "users",
+            localField: "author_id",
+            foreignField: "user_id",
+            as: "author_user",
+          },
+        },
+        {
+          $unwind: {
+            path: "$author_user",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+
+        // Join with user_profile to get profile_image
+        {
+          $lookup: {
+            from: "users_profile",
+            localField: "author_id",
+            foreignField: "user_id",
+            as: "author_profile",
+          },
+        },
+        {
+          $unwind: {
+            path: "$author_profile",
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+
+        // Combine into author object
+        {
+          $addFields: {
+            author: {
+              first_name: "$author_user.first_name",
+              profile_image: "$author_profile.profile_image",
+            },
+          },
+        },
+
+        // Remove raw joined arrays
+        {
+          $project: {
+            author_user: 0,
+            author_profile: 0,
+          },
+        },
+      ])
+      .toArray();
   }
 
   async search(params) {
